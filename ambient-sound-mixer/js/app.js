0e4d32ec010d81eedb4fec503bc49dbb86f7bccc
Mustaf Ahmed Jirow <mustephj8@gmail.com>
2025-10-20 19:31:51 +0300

    Added ambient-sound-mixer project inside Javascript-2025

--- ambient-sound-mixer/js/app.js
+++ ambient-sound-mixer/js/app.js
@@ -1,11 +1,12 @@
 import { sounds, defaultPresets } from "./soundData.js"
+import { SoundManager } from "./soundManager.js"
 
 class AmbientMixer{
     // Initialze dependencies and default state
 
     constructor() {
-        console.log('Initializing state....')
-        this.soundManager = null
+        
+        this.soundManager = new SoundManager()
         this.ui = null
         this.presetManager = null
         this.timer = null
@@ -15,13 +16,26 @@ class AmbientMixer{
 
     init() {
         try {
-            console.log('Initializing app.....')
+            // load All sound files
+            this.loadAllSound()
             this.isInitialized=true
             
         } catch (error) {
             console.error('Failed to initialize app:',error)
             
         }
+    }
+
+    // Load All sounf files
+
+    loadAllSound() {
+        sounds.forEach((sound) => {
+            const audioUrl = `audio/${sound.file}`
+            const success = this.soundManager.loadSound(sound.id, audioUrl)
+            if (!success) {
+                console.log(`Could not load sound: ${sound.name} from ${audioUrl}`)
+            }
+        })
     }
 }
 
